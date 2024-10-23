60914ee5cadd5598b81d46b0379cd5aea4a608d5
Zikaal <168971723+Zikaal@users.noreply.github.com>
2024-10-24 00:51:27 +0500

    Update logout.js

--- logout.js
+++ logout.js
@@ -5,5 +5,5 @@ document.getElementById('logout-btn').addEventListener('click', logout);
 function logout() {
     localStorage.removeItem('isLoggedIn'); // Remove login state
     alert('You have been logged out.'); // Notify user
-    window.location.href = 'login.html'; // Redirect to login page
+    window.location.href = 'Login.html'; // Redirect to login page
   }
